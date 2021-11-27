8d7f767ebb7a06431b41fdde2813c11bb401bcf8
psycho-baller <Ramim66809@gmail.com>
2021-11-28 03:55:53 +0400

    finished the basic stuff for this project

--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options  # for suppressing the browser
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options  # for suppressing the browser
+from selenium.webdriver import ActionChains
+
 import warnings
-
 from bs4 import BeautifulSoup as bs
+import webbrowser
 
-Options = Options()
-Options.add_argument("--headless")
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = Options()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument("--headless")
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
-    driver = webdriver.Firefox(
-        executable_path=r'C:\firefox_driver\geckodriver.exe')  # ,options=Options
-
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+# remove the options argument if u wanna see the browser open and perform the automated process
 
 # %%
 url = 'https://ucalgary.sona-systems.com'
@@ -32,49 +37,50 @@ driver.execute_script("arguments[0].click();", element)
 WebDriverWait(driver, 10).until(EC.presence_of_element_located(
     (By.ID, "lnkStudySignupLink"))).click()
 
-
 # %%
 html = driver.page_source
 home_page = 'https://ucalgary.sona-systems.com/'
 soup = bs(html, 'html.parser')
 table_row = soup.find('tr').parent.findNextSibling()
+study_links = table_row.findAll('a')
 
-try:
-    if table_row.find('span', {'id': 'ctl00_ContentPlaceHolder1_lblNoStudies'}).text == 'No studies are available at this time.':
-        driver.close()
-        exit('There are currently no studies, come back next time')
-except(AttributeError):
-    print("THERE'S A STUDY UP!")
-    study_links = table_row.findAll(
-        'a', {'id': 'ctl00_ContentPlaceHolder1_repStudentStudies_ctl15_HyperlinkStudentStudyInfo'})
+links = set()
+for link in study_links:
+    links.add(f'{home_page}{link.get("href")}')
 
-    links = []
-    for link in study_links:
-        links.append(f'{home_page}{link.get("href")}')
-
-    num_of_links = links.__len__()
-    print(f'there is {num_of_links} avalaible study') if num_of_links == 1 else print(
-        f'there are {num_of_links} avalaible studies')
-
-    # print(links)
-
+num_of_links = len(links)
+if num_of_links == 0:
+    driver.close()
+    exit('There are no studies available currently, see u later!')
+print(f'there is {num_of_links} available study') if num_of_links == 1 else print(
+    f'there are {num_of_links} available studies')
 
 # %%
-    for link in links:
-        driver.get(link)
-        link = driver.page_source
-        #s = requests.Session()
-        #link = s.get(link).content
+for link in links:
+    driver.get(link)
+    link = driver.page_source
 
-        soup2 = bs(link, 'html.parser')
-        description = soup2.find(
-            'span', {'id': 'ctl00_ContentPlaceHolder1_lblLongDesc'}).get_text(' ')
-        print(description)
-        if input("\nIf u wanna participate in this study, press Enter, if not, type any letter then press Enter and you will see the next avalable if there is any other") == '':
-            driver.find_element(
-                By.ID, 'ctl00_ContentPlaceHolder1_lnkNonAdmin').click()
-            driver.find_element(
-                By.ID, 'ctl00_ContentPlaceHolder1_repTimeSlots_ctl00_Submit_Button').click()
+    soup2 = bs(link, 'html.parser')
+    description = soup2.find(
+        'span', {'id': 'ctl00_ContentPlaceHolder1_lblLongDesc'}).get_text(' ')
+    print()
+    print(description)
+    if input("\nIf u wanna participate in this study, press Enter, if not, type any letter then press Enter and you will see the next available if there is any other") == '':
+        driver.find_element(
+            By.ID, 'ctl00_ContentPlaceHolder1_lnkNonAdmin').click()
+
+        element = driver.find_element(
+            By.ID, 'ctl00_ContentPlaceHolder1_repTimeSlots_ctl00_Submit_Button')
+        ActionChains(driver).click(element).perform()
+        # not sure if this^ works, it's supposed to press the final submit button to sign up
+
+        if input('You got signed up!, press Enter if u wanna start the research study, otherwise, type any letter then press Enter') == '':
+            study_link = driver.find_element(
+                By.ID, 'ctl00_ContentPlaceHolder1_lnkStudyPreview').get_attribute('href')
+            driver.close()
+            print('Enjoy!')
+            webbrowser.open(study_link)
         else:
-            pass
-        driver.close()
+            driver.close()
+            print(
+                'You should recieve an email anytime now with the research link, have a woderful day')
